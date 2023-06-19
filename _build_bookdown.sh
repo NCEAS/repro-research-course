52070a1a23ae5ac08bd7118d638d4e0f30b9f829
Matt Jones <gitcode@magisa.org>
2023-06-19 14:07:17 -0800

    Use BRANCH env variable in build script.

--- _build_bookdown.sh
+++ _build_bookdown.sh
@@ -1,33 +1,24 @@
 #!/bin/sh
 
-# this isn't really a bookdown build anymore, it builds the
-# nceas-training quarto book. more changes to come soon...
+# This shell script should be moved into the GHA workflow
+# There is no need for it to be an external script
+# I removed some of it (MBJ 2023-06-19), but didn't finish refactoring
+# the quarto build. We can probably use quarto actions now rather
+# than shell scripts (see https://github.com/quarto-dev/quarto-actions)
 set -e
 
-BRANCH=$1
-BRANCH='2023-06-delta'
+# Assume a BRANCH env variable is set, and the repo is already checked out
+[ -z "${BRANCH}" ] && echo "Exiting, please set BRANCH environment variable."; exit 1 
 
 export TOP
 TOP=$(pwd)
-
 if [ ! -d public/$BRANCH ]; then
     mkdir -p public/$BRANCH
 fi
 
-# Clone the lesson materials from the lessons repo
-#git clone https://github.com/NCEAS/nceas-training.git --branch ${BRANCH} --single-branch
-
 # Build the book
-echo "Building book"
-
 cd nceas-training/materials
-# Install deps
-#Rscript -e "install.packages('devtools')"
-#Rscript -e "devtools::install_deps()" # Installs book-specific R deps
-                                         # defined in DESCRIPTION file
-
 quarto render
-
 mv _book $BRANCH
 cp -R $BRANCH "$TOP/public"
 cd "$TOP"
